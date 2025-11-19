227354d4ba59fba862a908ba87350a8046a92c66
dami <midoks@163.com>
2025-11-20 02:25:13 +0800

    up

---
+++ internal/cmd/service.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"os"
+	"os/signal"
+
+	"github.com/urfave/cli"
+
+	"move86go/core"
+    "move86go/core/lagran"
+    "move86go/core/logx"
+)
+
+var Service = cli.Command{
+	Name:        "service",
+	Usage:       "this command start service",
+	Description: `start service`,
+	Action:      runService,
+	Flags: []cli.Flag{
+		stringFlag("config, c", "", "custom configuration file path"),
+	},
+}
+
+func runService(c *cli.Context) error {
+	logx.Info("Run")
+	if portData, err := core.FileRead("port.txt"); err == nil {
+		lagran.HttpPort = string(portData)
+	}
+	go lagran.Run()
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, os.Interrupt)
+	go func() {
+		<-sigs
+		lagran.UnsetIptable(lagran.HttpPort)
+		done <- true
+	}()
+	<-done
+	logx.Info("exit")
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
-    "os"
-    "os/signal"
-    
-    "move86go/core"
-    "move86go/core/lagran"
-    "move86go/core/logx"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+
+	"move86go/internal/cmd"
+
 )
 
+const (
+	Version = "1.0"
+	AppName = "move86go"
+)
+
+
 func main() {
-	logx.Info("Run")
-	if portData, err := core.FileRead("port.txt"); err == nil {
-		lagran.HttpPort = string(portData)
+	app := cli.NewApp()
+	app.Name = AppName
+	app.Version = Version
+	app.Usage = "move86go service"
+	app.Commands = []cli.Command{
+		cmd.Service,
 	}
-	go lagran.Run()
-	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
-	signal.Notify(sigs, os.Interrupt)
-	go func() {
-		<-sigs
-		lagran.UnsetIptable(lagran.HttpPort)
-		done <- true
-	}()
-	<-done
-	logx.Info("exit")
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatalf("Failed to start application: %v", err)
+	}
 }
